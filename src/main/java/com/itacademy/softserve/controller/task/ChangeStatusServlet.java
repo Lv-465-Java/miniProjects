143a191d6175599d1d56170c02748759088f61eb
sydor_artur <sydor.artur@ukr.net>
2020-01-13 09:51:55 +0200

    fix confirm delete in search servlet

--- src/main/java/com/itacademy/softserve/controller/task/ChangeStatusServlet.java
+++ src/main/java/com/itacademy/softserve/controller/task/ChangeStatusServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(ServletUrl.CHANGE_STATUS)
@@ -26,7 +27,8 @@ public class ChangeStatusServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.sendRedirect(req.getContextPath() + ServletUrl.HOME_URL);
+        HttpSession session = req.getSession(false);
+        resp.sendRedirect((String) session.getAttribute("referer"));
     }
 
     @Override
--- src/main/java/com/itacademy/softserve/controller/task/ConfirmDeleteServlet.java
+++ src/main/java/com/itacademy/softserve/controller/task/ConfirmDeleteServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(ServletUrl.CONFIRM_DELETE)
@@ -20,12 +21,13 @@ public class ConfirmDeleteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        session.setAttribute("referer", req.getHeader("Referer"));
         req.getRequestDispatcher(JspUrl.CONFIRM_DELETE_JSP).include(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-//        request.getRequestDispatcher(JspUrl.CONFIRM_DELETE_JSP).include(request, response);
     }
 }
